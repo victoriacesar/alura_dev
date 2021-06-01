502bec7f4a857ed27c75b4b8f777d6246ee0f308
Victoria Cesar <victoriacesaras@gmail.com>
2021-06-01 09:24:39 -0300

    change on save code area

--- community.js
+++ community.js
@@ -2,6 +2,7 @@ const mainCommunity = document.querySelector(".main_community_card");
 
 window.onload = function () {
   showProjects();
+  console.log(localStorage.length);
 };
 
 function showProjects() {
@@ -14,20 +15,22 @@ function showProjects() {
     projects.map((project) => {
       let card = cardStyle(project);
       mainCommunity.innerHTML += card;
+      let htmlCode = mainCommunity.querySelector(`[data-id="${project.id}"]`);
+      htmlCode.innerText = project.projectData.code;
     });
   }
 }
 
 function cardStyle(project) {
   const card = `
-  <div class="card_box id="${project.id}">
+  <div class="card_box">
     <div class="card_box-code" style="background: ${project.projectData.color}">
       <div class="code_circleBar">
         <div class="code_circle red"></div>
         <div class="code_circle yellow"></div>
         <div class="code_circle green"></div>
       </div>
-      <p class="p_textarea">${project.projectData.code}</p>
+      <p class="p_textarea" data-id="${project.id}"></p>
     </div>
     <div class="card_info">
       <h2>${project.projectData.name}</h2>
@@ -36,11 +39,11 @@ function cardStyle(project) {
     <div class="card_bottomBar">
       <div class="card_bottomBar_icon">
         <div class="count">
-          <i class="fas fa-comment"></i>
+          <img src="assets/img/comment.svg" alt="Comment" />
           <span>9</span>
         </div>
         <div class="count">
-          <i class="fas fa-heart"></i>
+          <img src="assets/img/heart.svg" alt="Heart" />
           <span>9</span>
         </div>
       </div>
